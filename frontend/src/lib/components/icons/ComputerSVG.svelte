f968beb5677159e2c76034bed9a2ce80d86926ca
taneshqGupta <f.taneshq@gmail.com>
2026-02-14 02:34:18 +0530

    update

---
+++ frontend/src/lib/components/icons/ComputerSVG.svelte
@@ -0,0 +1,19 @@
+<svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="12"
+    height="12"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="lucide lucide-monitor-down-icon lucide-monitor-down"
+    ><path d="M12 13V7" /><path d="m15 10-3 3-3-3" /><rect
+        width="20"
+        height="14"
+        x="2"
+        y="3"
+        rx="2"
+    /><path d="M12 17v4" /><path d="M8 21h8" /></svg
+>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,11 @@
+// vite.config.ts
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		host: '0.0.0.0',
+		port: 3000
+	}
 });
