c0a27515b32634d0b9f661152abf0977aee7b83c
taneshqGupta <f.taneshq@gmail.com>
2026-02-14 15:01:43 +0530

    update

--- frontend/src/lib/components/Logo.svelte
+++ frontend/src/lib/components/Logo.svelte
@@ -2,7 +2,7 @@
 	import { goto } from '$app/navigation';
 </script>
 
-<button on:click={() => goto('/')} class="cursor-pointer bg-transparent border-none p-2">
+<button on:click={() => goto('/')} class="cursor-pointer bg-transparent border-none p-4">
 	<h1 class="logo">AEGIS</h1>
 </button>
 
