1685b28d5a7bdd6857a8e06b6e02a78dcef751cc
taneshqGupta <f.taneshq@gmail.com>
2026-02-15 03:31:17 +0530

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,12 @@
 // vite.config.ts
 import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
 	plugins: [
+		tailwindcss(),
 		sveltekit(),
 		VitePWA({
 			registerType: 'autoUpdate',
