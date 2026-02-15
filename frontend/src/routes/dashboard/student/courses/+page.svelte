2851e8c88ade0e09e9214b6fcc90d948b30103dd
taneshqGupta <f.taneshq@gmail.com>
2026-02-16 00:48:12 +0530

    update

--- frontend/src/routes/dashboard/student/courses/+page.svelte
+++ frontend/src/routes/dashboard/student/courses/+page.svelte
@@ -115,15 +115,6 @@
         </div>
     {/if}
 
-    <button
-        class="submit-btn"
-        onclick={() => goto("/dashboard/student/courses/enroll")}
-        aria-label="Enroll in course"
-    >
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-        </svg>
-    </button>
 </div>
 
 <style>
